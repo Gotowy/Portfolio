482023efe6426a87a91305a5c268442b493203c1
Gotowy <pszygoda@gmail.com>
2022-03-07 08:11:46 +0100

    navbar and services improved, animations.scss added, minor changes

--- src/ts/navbar.ts
+++ src/ts/navbar.ts
@@ -1,14 +1,31 @@
-import { injectHtmlTemplate, addClass, removeClass } from './services';
+import { injectHtmlTemplate, addClass, removeClass, scroll } from './services';
 
 export const addNavbar = (): void => {
     injectHtmlTemplate('navbar.html', '.navbar');
-    document.querySelectorAll('.navBtn').forEach((item: Element): void => {
+    const homeButton: HTMLDivElement = document.querySelector('.home-btn') as HTMLDivElement;
+    const contentButtons: NodeListOf<HTMLLIElement> = document.querySelectorAll('.content-btn');
+
+    homeButton?.addEventListener('click', () => {    
+        removeClass('yellow', '.name');
+        addClass('yellow', '.name');
+        
+        removeClass('visible', '.title');
+        addClass('visible', '.title');
+        
+        removeClass('shade', '.bg');      
+        addClass('shade', '.bg');
+
+        removeClass('active', '.content-btn[class*="active"]');
+        scroll('.bg');
+    })
+
+    contentButtons?.forEach((item: Element): void => {
         const value = item.getAttribute('value');
         item.addEventListener('click', () => {
             injectHtmlTemplate(`${value}.html`, 'main');
-            removeClass('active', '.contentBtn[class*="active"]');
-            addClass('active', `.contentBtn[value="${value}"]`);
+            removeClass('active', '.content-btn[class*="active"]');
+            addClass('active', `.content-btn[value="${value}"]`);
+            scroll('main');
         });
     });
-    addClass('color', '.name');
 }
--- src/ts/services.ts
+++ src/ts/services.ts
@@ -1,15 +1,23 @@
 export const injectHtmlTemplate = (templateFileName: `${string}.html`, cssSelector: string) => {
     const container: HTMLElement = document.querySelector(cssSelector) as HTMLElement;
     const template: string = require(`../html/${templateFileName}`).default;
-    container.innerHTML = template;
+    if (container && template) container.innerHTML = template;
 }
 
 export const addClass = (className: string, cssSelector: string): void => {
-    const container: HTMLElement = document.querySelector(cssSelector) as HTMLElement;
-    setTimeout(() => container?.classList.add(className), 0);
+    const element: HTMLElement = document.querySelector(cssSelector) as HTMLElement;
+    setTimeout(() => element?.classList.add(className), 0);
 }
 
 export const removeClass = (className: string, cssSelector: string): void => {
-    const container: HTMLElement = document.querySelector(cssSelector) as HTMLElement;
-    container?.classList.remove(className);
+    const element: HTMLElement = document.querySelector(cssSelector) as HTMLElement;
+    element?.classList.remove(className);
+}
+
+export const scroll = (cssSelector: string): void => {
+    const element: HTMLElement = document.querySelector(cssSelector) as HTMLElement;
+    element?.scrollIntoView({
+        block: "start",
+        behavior: "smooth",
+    });
 }
